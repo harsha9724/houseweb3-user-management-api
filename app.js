3db281cf3f5d1f3069d3eb3bba8f1219c50a5f62
harsha9724 <harshahr9724@gmail.com>
2024-05-24 11:22:53 +0530

    connected to local mangoDB database

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express=require("express");
 const app=express();
 const cors = require('cors');
 const dotenv = require("dotenv");
+const mongoose=require("mongoose");
 dotenv.config()
 
 
 app.use(cors())
 app.use(express.json());
 
+const mangoURL = process.env.MANGO_URL
 
+mongoose.connect(`${mangoURL}/todos`);
+  
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+    console.log('Connected to MongoDB');
+  });
 
+app.get('/',(req,res)=>{
+    res.send("Server running successfully")
+})
 app.get("*", (req, res) => {
     res.status(404).send("404 PAGE NOT FOUND")
 })
